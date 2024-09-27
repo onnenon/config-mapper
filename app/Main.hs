63a5f99a373adb9c619cfdd7c950164655a7adb2
Stephen <stephen.onnen@gmail.com>
2024-09-26 22:47:54 -0500

    state things

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,18 @@
 module Main where
 
+import Control.Monad.State
+
+type Counter = Int
+
+increment :: State Counter Int
+increment = do
+  counter <- get
+  let newCount = counter + 1
+  put newCount
+  return newCount
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  let (result, finalState) = runState increment 0
+  putStrLn $ "Result: " ++ show result
+  putStrLn $ "Final state: " ++ show finalState
