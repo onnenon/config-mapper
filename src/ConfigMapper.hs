61d65f2cd474c56021ca33b6641623f299f48aea
Stephen <stephen.onnen@gmail.com>
2024-09-27 01:28:37 -0500

    type names

--- src/ConfigMapper.hs
+++ src/ConfigMapper.hs
@@ -4,14 +4,16 @@ import Data.Map (Map)
 import Data.Map qualified as M
 
 -- Type synonyms for better readability
-type OuterMap k1 k2 v = Map k1 (Map k2 v)
+type ConfigMap k1 k2 v = Map k1 (Map k2 v)
 
-type InnerMap k2 v k1 = Map k2 (Map v [k1])
+type InvertedConfigMap k2 v k1 = Map k2 (Map v [k1])
 
 type Tuple k1 k2 v = (k2, v, k1)
 
+type InvertedConfigMapValue k1 k2 v = (k2, Map v [k1])
+
 -- Convert the nested map into a list of tuples
-toListOfTuples :: (Ord k1, Ord k2, Ord v) => OuterMap k1 k2 v -> [Tuple k1 k2 v]
+toListOfTuples :: (Ord k1, Ord k2, Ord v) => ConfigMap k1 k2 v -> [Tuple k1 k2 v]
 toListOfTuples xs =
   [ (k2, v, k1)
     | (k1, kvs) <- M.toList xs,
@@ -19,13 +21,13 @@ toListOfTuples xs =
   ]
 
 -- Convert each tuple into the desired format
-convertTuple :: (Ord k1, Ord k2, Ord v) => Tuple k1 k2 v -> (k2, Map v [k1])
+convertTuple :: (Ord k1, Ord k2, Ord v) => Tuple k1 k2 v -> InvertedConfigMapValue k1 k2 v
 convertTuple (k2, v, k1) = (k2, M.singleton v [k1])
 
 -- Combine the tuples into the final map
-combineTuples :: (Ord k2, Ord v, Ord k1) => [(k2, Map v [k1])] -> InnerMap k2 v k1
+combineTuples :: (Ord k2, Ord v, Ord k1) => [InvertedConfigMapValue k1 k2 v] -> InvertedConfigMap k2 v k1
 combineTuples = M.fromListWith (M.unionWith (++))
 
 -- Invert the map
-invertMap :: (Ord k1, Ord k2, Ord v) => OuterMap k1 k2 v -> InnerMap k2 v k1
+invertMap :: (Ord k1, Ord k2, Ord v) => ConfigMap k1 k2 v -> InvertedConfigMap k2 v k1
 invertMap = combineTuples . map convertTuple . toListOfTuples
